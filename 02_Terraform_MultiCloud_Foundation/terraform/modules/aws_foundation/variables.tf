a45f6b9341b719b64e405195aef12804cbb736ec
Jamie Christian II <jamiechristian557@gmail.com>
2026-05-19 19:39:47 -0400

    Delete 02_Terraform_MultiCloud_Foundation directory

--- 02_Terraform_MultiCloud_Foundation/terraform/modules/aws_foundation/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "name_prefix" { type = string }
-variable "vpc_cidr" { type = string }
-variable "subnet_cidr" { type = string }
-variable "instance_type" { type = string }
-variable "bucket_name" { type = string }
-variable "allowed_ssh_cidr" { type = string }
